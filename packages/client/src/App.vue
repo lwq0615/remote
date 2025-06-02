4f4c9e3c1bd1ab0168252abcb7fa679cfa9bd109
liwq <18150027197@163.com>
2025-06-02 21:48:01 +0800

    router

--- packages/client/src/App.vue
+++ packages/client/src/App.vue
@@ -1,10 +1,10 @@
 <template>
   <a-layout style="height: 100vh">
-    <a-layout-header style="background: #001529; color: #fff;">
+    <!-- <a-layout-header style="background: #001529; color: #fff;">
       <div style="font-size: 20px; color: #fff;">remote</div>
-    </a-layout-header>
+    </a-layout-header> -->
     <a-layout>
-      <a-layout-sider v-model:collapsed="collapsed" collapsible width="120" style="background: #fff;">
+      <!-- <a-layout-sider v-model:collapsed="collapsed" collapsible width="120" style="background: #fff;">
         <a-menu
           mode="inline"
           :selectedKeys="[$route.path]"
@@ -17,7 +17,7 @@
             <router-link to="/git">Git操作</router-link>
           </a-menu-item>
         </a-menu>
-      </a-layout-sider>
+      </a-layout-sider> -->
       <a-layout-content style="padding: 12px; min-height: 280px;">
         <div class="bg-white h-full overflow-auto rounded-sm">
           <router-view />
--- packages/client/src/router.js
+++ packages/client/src/router.js
@@ -2,6 +2,10 @@ import { createRouter, createWebHistory } from 'vue-router';
 
 const routes = [
   {
+    path: '/',
+    redirect: '/git',
+  },
+  {
     path: '/git',
     name: 'Git',
     component: () => import('./views/git.vue'),
