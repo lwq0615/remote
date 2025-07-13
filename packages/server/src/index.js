336ab611a44a0f85100cc6ee14af2bf94d287d49
liwq <18150027197@163.com>
2025-07-13 16:40:15 +0800

    mac

--- packages/server/src/index.js
+++ packages/server/src/index.js
@@ -1,71 +1,75 @@
-import express from 'express';
-import exec, { getExecContext } from './exec.js';
-import { startWs } from './scoket.js';
-import fs from 'fs';
+import express from "express";
+import exec, { getExecContext } from "./exec.js";
+import { startWs } from "./scoket.js";
+import os from "os";
 export const app = express();
+import path from 'path'
 const port = 8899;
+const platform = os.platform();
+const isMac = platform === "darwin";
 
 const vpnConfig = {
   1: {
-    name: 'ql-L2TP',
-    username: 'liwq-L2TP',
-    password: 'Liwq@20230722',
+    name: "ql-L2TP",
+    username: "liwq-L2TP",
+    password: "Liwq@20230722",
   },
   2: {
-    name: 'ql-PPTP',
-    username: 'liwq',
-    password: 'Liwq@20230722',
+    name: "ql-PPTP",
+    username: "liwq",
+    password: "Liwq@20230722",
   },
 };
-let cwd = 'D:\\projects';
-cwd = fs.existsSync(cwd) ? cwd : 'D:\\project';
-const workspace = 'cd ' + cwd;
+let cwd = path.resolve(process.cwd(), '../../..')
+const workspace = "cd " + cwd;
 
 function getWorkspace(project) {
-  return `${workspace}\\${project}`;
+  return `${workspace}/${project}`;
 }
 
 // 检查 VPN 是否已连接
-async function checkVPNStatus(vpn) {
+async function checkVPNStatus(vpn = 1) {
   try {
-    const stdout = await exec('rasdial');
-    if (!vpn) {
-      return !stdout.includes('没有连接');
-    }
+    // networksetup -showpppoestatus ql-L2TP
     const vpnName = vpnConfig[vpn].name;
-    return stdout.includes(vpnName);
+    const stdout = await exec(
+      isMac ? "networksetup -showpppoestatus " + vpnName : "rasdial"
+    );
+    if (!vpn) {
+      return !stdout.includes("没有连接");
+    }
+    return isMac ? (stdout.trim() === 'connected') : stdout.includes(vpnName);
   } catch (error) {
-    console.error('检查 VPN 状态时出错:', error);
+    console.error("检查 VPN 状态时出错:", error);
     throw {
-      message: '检查 VPN 状态时出错',
+      message: "检查 VPN 状态时出错",
       error: error,
     };
   }
 }
 
 // 连接 VPN
-async function connectVPN(vpn) {
+async function connectVPN(vpn = 1) {
   const { name, username, password } = vpnConfig[vpn];
   try {
-    const connectCommand = `rasdial "${name}" "${username}" "${password}"`;
+    const connectCommand = isMac ? `networksetup -connectpppoeservice ${name}` : `rasdial "${name}" "${username}" "${password}"`;
     const stdout = await exec(connectCommand);
-    if (stdout.includes('已连接')) {
-      console.log('VPN 连接成功:', stdout);
+    if (isMac || stdout.includes("已连接")) {
+      console.log("VPN 连接成功:", stdout);
       return true;
     } else {
-      console.error('VPN 连接失败:', stdout);
-      throw new Error('VPN 连接失败');
+      console.error("VPN 连接失败:", stdout);
+      throw new Error("VPN 连接失败");
     }
   } catch (error) {
-    console.error('VPN 连接失败:', error);
+    console.error("VPN 连接失败:", error);
     throw {
-      message: 'VPN 连接失败',
+      message: "VPN 连接失败",
       error: error,
     };
   }
 }
 
-// 执行 Git 命令
 async function executeGitCommands(project, branch) {
   const commands = [
     `${getWorkspace(project)} && git pull origin2 master`,
@@ -89,13 +93,13 @@ async function pushCode(vpn, project, branch) {
   try {
     const isConnected = await checkVPNStatus(vpn);
     if (!isConnected) {
-      console.log('VPN 未连接，开始连接...');
+      console.log("VPN 未连接，开始连接...");
       await connectVPN(vpn);
     } else {
-      console.log('VPN 已连接');
+      console.log("VPN 已连接");
     }
 
-    console.log('开始执行 Git 命令...');
+    console.log("开始执行 Git 命令...");
     await executeGitCommands(project, branch);
   } finally {
     return endListen();
@@ -103,48 +107,56 @@ async function pushCode(vpn, project, branch) {
 }
 
 // 主接口
-app.post('/git/push', async (req, res) => {
-  const { vpn = 1, project = 'ql-new-cloud', branch = 'feature_v1.1.0' } = JSON.parse(req.query.params);
+app.post("/git/push", async (req, res) => {
+  const {
+    vpn = 1,
+    project = "ql-new-cloud",
+    branch = "feature_v1.1.0",
+  } = JSON.parse(req.query.params);
   pushCode(vpn, project, branch)
     .then((data) => {
-      res.status(200).json({ message: '操作成功', data });
+      res.status(200).json({ message: "操作成功", data });
     })
     .catch((error) => {
       res.status(500).json(error);
     });
 });
 
-app.get('/git/push', async (req, res) => {
-  const { vpn = 1, project = 'ql-new-cloud', branch = 'master' } = req.query;
+app.get("/git/push", async (req, res) => {
+  const { vpn = 1, project = "ql-new-cloud", branch = "master" } = req.query;
   pushCode(vpn, project, branch)
     .then((data) => {
-      res.status(200).json({ message: '操作成功', data });
+      res.status(200).json({ message: "操作成功", data });
     })
     .catch((error) => {
       res.status(500).json(error);
     });
 });
 
-app.get('/git/branch', async (req, res) => {
-  const { project = 'ql-new-cloud' } = req.query;
+app.get("/git/branch", async (req, res) => {
+  const { project = "ql-new-cloud" } = req.query;
   const stdout = await exec(`${getWorkspace(project)} && git branch`);
   res.status(200).json(stdout);
 });
 
-app.get('/git/checkout', async (req, res) => {
-  const { project = 'ql-new-cloud', branch } = req.query;
+app.get("/git/checkout", async (req, res) => {
+  const { project = "ql-new-cloud", branch } = req.query;
   if (!branch) {
     res.status(500).json({
-      message: '分支名称不能为空',
+      message: "分支名称不能为空",
     });
   }
   const stdout = await exec(`${getWorkspace(project)} && git branch`);
   // 检查分支是否存在
-  let exist = stdout.split('\n').some((item) => item.replaceAll('*', '').trim() === branch);
+  let exist = stdout
+    .split("\n")
+    .some((item) => item.replaceAll("*", "").trim() === branch);
   try {
-    await exec(`${getWorkspace(project)} && git checkout ${exist ? '' : '-b '}${branch}`);
+    await exec(
+      `${getWorkspace(project)} && git checkout ${exist ? "" : "-b "}${branch}`
+    );
     res.status(200).json({
-      message: '操作成功',
+      message: "操作成功",
     });
   } catch (e) {
     res.status(500).json({
@@ -154,26 +166,28 @@ app.get('/git/checkout', async (req, res) => {
   }
 });
 
-app.get('/git/delete', async (req, res) => {
-  const { project = 'ql-new-cloud', branch } = req.query;
+app.get("/git/delete", async (req, res) => {
+  const { project = "ql-new-cloud", branch } = req.query;
   if (!branch) {
     res.status(500).json({
-      message: '分支名称不能为空',
+      message: "分支名称不能为空",
     });
   }
   const stdout = await exec(`${getWorkspace(project)} && git branch`);
   // 检查分支是否存在
-  let exist = stdout.split('\n').some((item) => item.replaceAll('*', '').trim() === branch);
+  let exist = stdout
+    .split("\n")
+    .some((item) => item.replaceAll("*", "").trim() === branch);
   if (!exist) {
     res.status(500).json({
-      message: '分支不存在',
+      message: "分支不存在",
     });
     return;
   }
   try {
     await exec(`${getWorkspace(project)} && git branch -d ${branch}`);
     res.status(200).json({
-      message: '操作成功',
+      message: "操作成功",
     });
   } catch (e) {
     res.status(500).json({
@@ -191,7 +205,7 @@ app.listen(port, () => {
   setInterval(() => {
     checkVPNStatus().then((isConnected) => {
       if (!isConnected) {
-        console.log('VPN 未连接，尝试重新连接...');
+        console.log("VPN 未连接，尝试重新连接...");
         connectVPN(1).catch((error) => {
           console.error(error);
         });
